4ed06b676be88a3f1861b5757602631025a1ed44
Ilya Danilkin <nezorflame@gmail.com>
2018-10-23 14:33:24 +0300

    conventional errors

--- config/errors.go
+++ config/errors.go
@@ -4,20 +4,24 @@ import (
 	"errors"
 )
 
+// Common errors
 var (
-	ErrNotImplemented   = errors.New("Not implemented")
-	ErrVersionNotFound  = errors.New("Version not found")
-	ErrVariableNotFound = errors.New("Variable not found")
+	ErrNotImplemented   = errors.New("not implemented")
+	ErrVersionNotFound  = errors.New("version not found")
+	ErrVariableNotFound = errors.New("variable not found")
 )
 
+// IsNotImplemented checks if the provider error equals ErrNotImplemented
 func IsNotImplemented(err error) bool {
 	return err == ErrNotImplemented
 }
 
+// IsVersionNotFound checks if the provider error equals ErrVersionNotFound
 func IsVersionNotFound(err error) bool {
 	return err == ErrVersionNotFound
 }
 
+// IsVariableNotFound checks if the provider error equals ErrVariableNotFound
 func IsVariableNotFound(err error) bool {
 	return err == ErrVariableNotFound
 }
